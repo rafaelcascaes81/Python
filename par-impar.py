f41d26d5ef3d77a94179fa9b66465145c8be775a
rafael <rafaelcascaes81@gmail.com>
2025-03-31 11:44:29 -0300

    Create par-impar.py
    Exercício Python para saber se o numero é par ou impar.

---
+++ par-impar.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira um valor inteiro
+N = int(input("Digite um número inteiro: "))
+
+# Verifica se N é par ou ímpar
+if N % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar") 
