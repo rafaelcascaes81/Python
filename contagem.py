f61ffcfc7b708761959ad47366ec50b6d8cd2af0
rafael <rafaelcascaes81@gmail.com>
2025-03-31 19:45:23 -0300

    Add files via upload

---
+++ contagem.py
@@ -0,0 +1,12 @@
+print("Contagem regressiva")
+cont = 16
+while cont > 0:
+    cont = cont - 1
+    print(cont)
+
+print("contagem progressiva")
+cont2 = 0
+while cont2 < 16:
+    print(cont2)   
+    cont2 = cont2 + 1
+    #print(cont2)
